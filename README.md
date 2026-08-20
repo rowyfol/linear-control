ee82cc73eb2e3bad4db4649d6ce06c94c179e0c2
rowyfol <193430696+rowyfol@users.noreply.github.com>
2026-08-20 22:52:11 +0330

    readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-List of every file with description:
+How to run it?
+
+first
+```bash
+julia -e 'import Pkg; Pkg.add("Pluto")'
+```
+
+then
+```bash
+julia -e 'import Pluto; Pluto.run()'
+```
--- test.jl
+++ test.jl
@@ -4,8 +4,26 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    return quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
+
 # ╔═╡ 4a0f2b50-9caa-11f1-9540-ff4b962b4c27
 println("Hi")
+
+# ╔═╡ 8fa32346-e8cf-42c9-8768-8c3843a94f20
+@bind a html"<input type=range >"
+
+# ╔═╡ 855046ec-49b4-4af8-8715-48842ae6f180
+println(a)
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -25,5 +43,7 @@ project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
 
 # ╔═╡ Cell order:
 # ╠═4a0f2b50-9caa-11f1-9540-ff4b962b4c27
+# ╠═8fa32346-e8cf-42c9-8768-8c3843a94f20
+# ╠═855046ec-49b4-4af8-8715-48842ae6f180
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
