96fd8893af6d33c03625e2782b7acd35e668a759
rowyfol <193430696+rowyfol@users.noreply.github.com>
2026-08-20 18:49:17 +0330

    switch from jupter to Pluto.jl

---
+++ test.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v1.0.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4a0f2b50-9caa-11f1-9540-ff4b962b4c27
+println("Hi")
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.6"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═4a0f2b50-9caa-11f1-9540-ff4b962b4c27
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
